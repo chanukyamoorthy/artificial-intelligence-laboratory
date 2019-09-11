77436108f1258a1105541b33894849f8bbd0fee3
Adhinarayana Moorthy Chanukya <adinarayanamoorthychanukya@gmail.com>
2019-09-11 11:27:57 +0530

    second-commit

--- eightpuzzle.cpp
+++ eightpuzzle.cpp
@@ -1,171 +1,136 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int goal[3][3] {{1,2,3},{4,5,6},{7,8,-1}};
- 
 struct node{
-	int currentState[3][3];
-	struct node *parent;
-	int h;
-	int f;	
+	int board[3][3], cost,g;
+	int x,y; //coordinates of blank tile
+	node* parent;
 };
 
-struct compare_fscore{
-	bool operator()(node const &p1, node const &p2)
-	{
-		return p2.f<p1.f;
-	}
-};
+int row[] = { 1, 0, -1, 0 }; 
+int col[] = { 0, -1, 0, 1 }; //all 4 movement of a tile
 
-int calculate_hScore(int misplaced[3][3])
-{
-	int hScore=0;
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			if(misplaced[i][j]!=-1 && misplaced[i][j]!=goal[i][j])
-				hScore++;
-		}
-	}	
-	return hScore;
-}
-
-struct node newNode(int newState[3][3], struct node *ptr, int height)
-{
-	struct node temp;
-	for(int i=0;i<3;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			temp.currentState[i][j]=newState[i][j];
+int hamming(int ar[3][3])
+{   
+	int i,j,count=1,h=0;
+	for (i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			if(i+j!=4 && count!=ar[i][j])
+				h++;
+			count++;
 		}
 	}
-	temp.parent=ptr;
-	temp.h=height;
-	temp.f=calculate_hScore(newState)+height;
-	return temp;
+	return h;
 }
 
-/*
-void recurse(struct node *root)
-{
-	if(root==NULL)
-		return;
-	recurse(root->parent);
-	for(int j=0;j<3;j++)
-	{
-		for(int k=0;k<3;k++)
-		{
-			cout<<root->currentState[j][k]<<" ";
+//to get new state
+node* nState(int mat[3][3], int xi, int yi, int xf, int yf, int g, node* parent) 
+{ 
+    node* n = new node; 
+    n->parent = parent; 
+  
+    memcpy(n->board, mat, sizeof(n->board)); 
+    //slide tile by 1 position 
+    swap(n->board[xi][yi], n->board[xf][yf]); 
+  
+    n->cost = INT_MAX; 
+  
+    //set number of moves so far 
+    n->g = g; 
+
+    n->x = xf; 
+    n->y = yf; 
+    return n; 
+} 
+
+void printbrd(int ar[3][3]){
+	int i,j;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			if(ar[i][j]!=0)
+				cout<<ar[i][j]<<" ";
+			else
+				cout<<"  ";
 		}
 		cout<<endl;
 	}
-	cout<<"g= "<<root->h<<", h= "<<root->f-root->h<<", f= "<<root->f<<endl;
-	cout<<endl;
-	
-	
 }
 
-void printSolution(vector <node> closeList)
-{
-	struct node temp=closeList[closeList.size()-1];
-	for(int j=0;j<3;j++)
+//recursion to print the sequence
+void printPath(node* root) 
+{ 
+    if (root == NULL) 
+        return; 
+    printPath(root->parent); 
+    printbrd(root->board);
+    cout<<"g : "<<root->g<<"   "<<" h : "<<root->cost<<"   "<<"Cost : "<<root->g+root->cost<<endl;
+    cout<<endl; 
+} 
+
+struct calcf 
+{ 
+    bool operator()(const node* m, const node* n) const
+    { 
+    	//f=g+h
+        return (m->cost + m->g) > (n->cost + n->g); 
+    } 
+}; 
+
+int valid(int x, int y) 
+{ 
+    return (x>=0 && x<3 && y>=0 && y<3); 
+} 
+
+int main(){
+	
+	int board[3][3]={{1,2,3},{0,4,6},{7,5,8}};
+	int x=1,y=0; // position of the blank
+	cout<<"Parent : "<<endl<<endl;
+	printbrd(board);
+	
+	priority_queue<node*, vector<node*>, calcf> explore;
+	
+	node* root = nState(board, x, y, x, y, 0, NULL); 
+    root->cost = hamming(board); 
+	explore.push(root); 
+    int iter=0;
+    while (!explore.empty())
 	{
-		for(int k=0;k<3;k++)
-		{
-			cout<<temp.currentState[j][k]<<" ";
-		}
-		cout<<endl;
+		/*
+    	cout<<"Iteration #"<<iter<<endl;
+    	iter++;
+    	if (iter==11)
+    		break
+    	*/
+    	
+    	node* min = explore.top(); 
+        explore.pop(); 
+       
+        if (min->cost == 0) 
+        { 
+            cout<<"Path : "<<endl<<endl<<endl;
+            printPath(min); 
+            break; 
+        } 
+  
+        
+        // min is the node popped from the priority queue
+        cout<<"Children at level  :"<<iter<<endl<<endl<<endl;
+        for (int i=0;i<4;i++) 
+        { 
+            if (valid(min->x+row[i], min->y+col[i])) 
+            { 
+                
+                node* child = nState(min->board, min->x, min->y, min->x + row[i], min->y + col[i], min->g + 1, min); 
+                child->cost = hamming(child->board);
+                
+            	printbrd(child->board);
+            	
+            	cout<<endl<<endl;
+                explore.push(child); 
+            } 
+        } 
+        iter++;
 	}
-	struct node *ptr=&temp;
-	recurse(ptr);	
-	
+
+	return 0;
 }
-*/
-
-void printSolution(vector <node> closeList)
-{
-	for(int i=0;i<closeList.size();i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			for(int k=0;k<3;k++)
-			{
-				cout<<closeList[i].currentState[j][k]<<" ";
-			}
-			cout<<endl;
-		}
-		cout<<"g= "<<closeList[i].h<<", h= "<<closeList[i].f-closeList[i].h<<", f= "<<closeList[i].f<<endl;
-		cout<<endl;
-	}	
-}
-
-
-void solvePuzzle(int currentState[3][3])
-{
-	priority_queue<node,vector<node>,compare_fscore> openList;
-	vector <node> closeList;
-	openList.push(newNode(currentState,NULL,0));
-	while(!openList.empty())
-	{
-		struct node optimal=openList.top();
-		if(calculate_hScore(optimal.currentState)==0)
-		{
-			openList.pop();
-			closeList.push_back(optimal);
-			break;	
-		}
-		openList.pop();
-		closeList.push_back(optimal);
-		for(int i=0;i<3;i++)
-		{
-			for(int j=0;j<3;j++)
-			{
-				if(optimal.currentState[i][j]==-1)
-				{
-					//swap up
-					if(i-1>=0)
-					{
-						swap(optimal.currentState[i][j],optimal.currentState[i-1][j]);
-						openList.push(newNode(optimal.currentState,&optimal,optimal.h+1));	
-						swap(optimal.currentState[i][j],optimal.currentState[i-1][j]);
-					}
-					//swap down
-					if(i+1<3)
-					{
-						swap(optimal.currentState[i][j],optimal.currentState[i+1][j]);
-						openList.push(newNode(optimal.currentState,&optimal,optimal.h+1));	
-						swap(optimal.currentState[i][j],optimal.currentState[i+1][j]);
-					}
-					//swap left
-					if(j-1>=0)
-					{
-						swap(optimal.currentState[i][j],optimal.currentState[i][j-1]);
-						openList.push(newNode(optimal.currentState,&optimal,optimal.h+1));	
-						swap(optimal.currentState[i][j],optimal.currentState[i][j-1]);
-					}
-					//swap right
-					if(j+1<3)
-					{
-						swap(optimal.currentState[i][j],optimal.currentState[i][j+1]);
-						openList.push(newNode(optimal.currentState,&optimal,optimal.h+1));	
-						swap(optimal.currentState[i][j],optimal.currentState[i][j+1]);
-					}
-					
-					break;
-				}
-			}
-		}
-		
-	}
-	printSolution(closeList);
-}
-
-
-int main()
-{
-	int currentState[3][3]={{1,2,3},{-1,4,6},{7,8,5}};
-	solvePuzzle(currentState);
-}
-
